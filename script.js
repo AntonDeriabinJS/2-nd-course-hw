b0844cf1df851aa47d71c5adb1a6fafb6d762f33
Антон Дерябин <deriabin@inbox.ru>
2026-05-31 06:51:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Задание 2
+let iphoneYear = 2007;
+alert(iphoneYear);
+
+// Задание 3
+let jsCreator = 'Брендан Эйх';
+alert(jsCreator);
+
+// Задание 4
+let num1 = 10;
+let num2 = 2;
+alert('Сумма: ' + (num1 + num2));
+alert('Разность: ' + (num1 - num2));
+alert('Произведение: ' + (num1 * num2));
+alert('Частное: ' + (num1 / num2));
+
+// Задание 5
+let result = 2 ** 5;
+alert(result);
+
+// Задание 6
+let a6 = 9;
+let b6 = 2;
+alert(a6 % b6);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 8
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+// Задание 9
+let user = {
+    name: 'Антон',
+    age: 50,
+    isAdmin: true
+};
+
+// Задание 10
+let userName = prompt('Введите ваше имя');
+alert(`Привет, ${userName}!`);
